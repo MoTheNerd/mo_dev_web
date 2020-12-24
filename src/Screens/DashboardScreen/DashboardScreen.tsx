a4edcf04be98e482ceff83927d7279c5d460a29d
Mohammad Al-Ahdal <mo@mohammad.dev>
2020-12-24 02:21:15 -0700

    add logout button for dashboard screen as well as description of milestone goals

--- src/Screens/DashboardScreen/DashboardScreen.tsx
+++ src/Screens/DashboardScreen/DashboardScreen.tsx
@@ -8,19 +8,34 @@ import { config } from '../../Config';
 export class DashboardScreen extends React.Component {
 
   async componentDidMount() {
-    let result:IAuthToken | string = await axios.get(`${config.AUTH_SERVER_ENDPOINT}/check?token=${getState("mohammad.dev.auth.authToken", "persist")}`)
+    let result: IAuthToken | string = await axios.get(`${config.AUTH_SERVER_ENDPOINT}/check?token=${getState("mohammad.dev.auth.authToken", "persist")}`)
 
-    subscribeToState("mohammad.dev.auth.authToken", "DashboardScreen", () => {this.forceUpdate.bind(this)()}, "persist")
+    subscribeToState("mohammad.dev.auth.authToken", "DashboardScreen", () => { this.forceUpdate.bind(this)() }, "persist")
 
     if (typeof result === "string") {
       setState("mohammad.dev.auth.authToken", null, "persist")
     }
   }
-    
+
   render() {
     return (
       <DashboardScreenContainer>
-        
+        <p>Welcome to your dashboard, Mohammad!</p>
+        <p>This will eventually be populated with the following features:</p>
+        <div>
+          <ul>
+            <li>Photo Upload (img, iso, aperture, focalLength, shutterSpeed, locationAndTime)</li>
+            <li>Portfolio Upload (img, markdown editor)</li>
+            <li>WorkExperience Upload (startTime, endTime[NULLABLE], description, bulletPoints)</li>
+            <li>Skills adder (basic string array)</li>
+            <li>ResumeGeneratorButton</li>
+          </ul>
+        </div>
+        <p>Seems exciting! This is a lot of work indeed - in the meanwhile you can still Logout and mess around with the mo_dev_auth microservice by logging out and logging in!</p>
+        <StyledLogoutButton onClick={() => {
+            setState("mohammad.dev.auth.authToken", null, "persist")
+            window.location.assign("/login")
+          }}>Log Out</StyledLogoutButton>
       </DashboardScreenContainer>
     );
   }
@@ -31,4 +46,18 @@ const DashboardScreenContainer = styled.div`
   justify-content: center;
   align-items: center;
   margin-top: 300px;
+  flex-direction: column;
+`
+
+const StyledLogoutButton = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: 20px;
+  border-radius: 0px;
+  color: #FFFFFF;
+  font-family: "Open Sans";
+  font-weight: 300;
+  font-size: 20px;
+  background-color: #000000;
 `
